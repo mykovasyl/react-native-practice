46016e1b429fff47f440da168e00ad53ec29f906
Mykola Vasyl'yev <mykovasyl@gmail.com>
2023-08-07 18:02:58 -0400

    second commit

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
 
 function App() {
   const placeholderImage = require("./assets/images/background-image.png");
@@ -9,6 +11,11 @@ function App() {
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={placeholderImage} />
       </View>
+      <View style={styles.footerContainer}>
+        {/* <Button theme='primary' label='Choose a photo' />
+        <Button label='Use this photo' /> */}
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -27,6 +34,10 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
 
